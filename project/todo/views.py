7f110db755fccc31bb26e6492019e62d3d2c55a6
Fernando Arroliga <luisfernandomorales930@gmail.com>
2024-01-05 20:11:08 -0600

    "Add pagination and style"

--- project/todo/views.py
+++ project/todo/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render, redirect
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
 from .models import Tarea
 
 from .forms import TareaForm
 
 # Create your views here.
 def home(request):
+    """
     tareas = Tarea.objects.all()
     context = {'tareas': tareas}
     return render(request, 'todo/home.html', context)
+    """
+
+    tareas_list = Tarea.objects.all()
+    paginator = Paginator(tareas_list, 4)  # Show 4 items per page
+
+    page = request.GET.get('page')
+    try:
+        tareas = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver the first page.
+        tareas = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g., 9999), deliver the last page.
+        tareas = paginator.page(paginator.num_pages)
+
+    context = {'tareas': tareas}
+    return render(request, 'todo/home.html', context)
+
 
 def agregar(request):
 
